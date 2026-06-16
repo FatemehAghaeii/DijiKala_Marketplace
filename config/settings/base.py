a791e3153993c2d45c5007ad3b04cfc7e764a119
FatemehAghaeii <fatemehaghaei1382@yahoo.com>
2026-06-16 07:47:46 +0330

    Configure global templates and static directories and add frontend assets

--- config/settings/base.py
+++ config/settings/base.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +115,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'account.User'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
